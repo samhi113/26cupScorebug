d787bed313c7ba0f1621fdecfe698708beb3670c
Sam Higgins <86005303+samhi113@users.noreply.github.com>
2026-07-24 16:56:31 -0400

    Refactor team color values and jersey selection logic
    Updated team color values for various countries and added jersey selection validation.

--- js/app.js
+++ js/app.js
@@ -15,65 +15,65 @@ const awayJer = document.getElementById("awayJer");
     const scoreRight = document.getElementById("scoreRight");
 
 const teamVals = {
-    "MEX": [["#185", "#fff"], ["#fff", "#032"], ["#000", "#185"]],
-    "RSA": [["#fe0", "#084"], ["#243", "#fff"]],
-    "KOR": [["#e13", "#fff"], ["#87c", "#fff"]],
-    "CZE": [["#b00", "#113"], ["#ddd", "#da4"]],
+    "MEX": [["#185", "#eee"], ["#eee", "#032"], ["#111", "#1a6"]],
+    "RSA": [["#ec0", "#074"], ["#243", "#eee"]],
+    "KOR": [["#c34", "#eee"], ["#87c", "#eee"]],
+    "CZE": [["#d12", "#113"], ["#ddd", "#da4"]],
 
-    "SUI": [["#e00", "#fff"], ["#9f6", "#467"]],
-    "CAN": [["#f00", "#fff"], ["#000", "#bbb"], ["#fff", "#444"]],
-    "BIH": [["#027", "#fff"], ["#fff", "#027"]],
-    "QAT": [["#902", "#fff"], ["#fff", "#902"]],
+    "SUI": [["#f00", "#fff"], ["#9f6", "#467"]],
+    "CAN": [["#d23", "#fff"], ["#111", "#bbb"], ["#eee", "#444"]],
+    "BIH": [["#027", "#eee"], ["#eee", "#027"]],
+    "QAT": [["#902", "#eee"], ["#eee", "#902"]],
 
-    "BRA": [["#fe4", "#000"], ["#000", "#359"]],
-    "MAR": [["#d22", "#084"], ["#fff", "#c96"]],
-    "SCO": [["#004", "#fff"], ["#f55", "#538"]],
-    "HAI": [["#00f", "#fff"], ["#fff", "#00f"], ["#f00", "#fff"]],
+    "BRA": [["#fe4", "#111"], ["#111", "#359"]],
+    "MAR": [["#b23", "#ddd"], ["#eee", "#c96"]],
+    "SCO": [["#004", "#eee"], ["#f55", "#538"]],
+    "HAI": [["#00d", "#eee"], ["#eee", "#00d"], ["#d00", "#eee"]],
 
-    "USA": [["#fff", "#f00"], ["#234", "#fff"]],
-    "AUS": [["#fb0", "#054"], ["#054", "#fff"]],
-    "PAR": [["#fff", "#f00"], ["#013", "#fff"]],
-    "TUR": [["#f00", "#fff"], ["#fff", "#f00"]],
+    "USA": [["#eee", "#a24"], ["#136", "#eee"]],
+    "AUS": [["#fb0", "#054"], ["#054", "#eee"]],
+    "PAR": [["#eee", "#f00"], ["#013", "#eee"]],
+    "TUR": [["#f00", "#eee"], ["#eee", "#f00"]],
 
-    "GER": [["#fff", "#000"], ["#013", "#7fe"]],
-    "CIV": [["#f60", "#070"], ["#fff", "#f60"]],
-    "ECU": [["#fd0", "#013"], ["#013", "#ca7"], ["#fff", "#013"]],
-    "CUW": [["#03e", "#ff0"], ["#ffb", "#06d"]],
+    "GER": [["#eee", "#111"], ["#013", "#7fe"]],
+    "CIV": [["#f60", "#070"], ["#eee", "#f60"]],
+    "ECU": [["#ec0", "#013"], ["#013", "#ca7"], ["#eee", "#013"]],
+    "CUW": [["#06d", "#ee0"], ["#ffb", "#06d"]],
 
-    "NED": [["#f53", "#000"], ["#fff", "#f53"]],
-    "JPN": [["#007", "#fff"], ["#eed", "#222"]],
-    "SWE": [["#fe0", "#00e"], ["#149", "#fe6"]],
-    "TUN": [["#fff", "#f00"], ["#f00", "#fff"], ["#000", "#fff"]],
+    "NED": [["#f53", "#111"], ["#eee", "#f53"]],
+    "JPN": [["#007", "#eee"], ["#eed", "#222"]],
+    "SWE": [["#fe0", "#11d"], ["#149", "#fe6"]],
+    "TUN": [["#eee", "#f00"], ["#f00", "#eee"], ["#111", "#eee"]],
 
-    "BEL": [["#f00", "#000"], ["#9df", "#000"]],
-    "EGY": [["#f01", "#000"], ["#fff", "#000"]],
-    "IRN": [["#fff", "#f00"], ["#f00", "#fff"]],
-    "NZL": [["#fff", "#000"], ["#000", "#fff"]],
+    "BEL": [["#f00", "#111"], ["#9df", "#111"]],
+    "EGY": [["#f01", "#111"], ["#eee", "#111"]],
+    "IRN": [["#eee", "#f00"], ["#f00", "#eee"]],
+    "NZL": [["#eee", "#111"], ["#111", "#eee"]],
 
-    "ESP": [["#e00", "#fc1"], ["#eed", "#523"]],
-    "CPV": [["#008", "#fff"], ["#000", "#008"], ["#f00", "#fff"]],
-    "URU": [["#9ce", "#000"], ["#102", "#45d"]],
-    "KSA": [["#043", "#fff"], ["#fff", "#043"]],
+    "ESP": [["#a11", "#eb1"], ["#edb", "#523"]],
+    "CPV": [["#008", "#eee"], ["#111", "#eee"], ["#f00", "#eee"]],
+    "URU": [["#9ce", "#111"], ["#102", "#9ce"]],
+    "KSA": [["#043", "#eee"], ["#eee", "#043"]],
 
-    "FRA": [["#126", "#fff"], ["#dfe", "#d60"]],
-    "NOR": [["#f00", "#025"], ["#000", "#fff"], ["#fff", "#000"]],
-    "SEN": [["#fff", "#065"], ["#065", "#ff0"]],
-    "IRQ": [["#fff", "#000"], ["#285", "#fff"], ["#000", "#fff"]],
+    "FRA": [["#126", "#eee"], ["#dfe", "#d60"]],
+    "NOR": [["#f00", "#025"], ["#111", "#eee"], ["#eee", "#111"]],
+    "SEN": [["#eee", "#065"], ["#065", "#ff0"]],
+    "IRQ": [["#eee", "#111"], ["#285", "#eee"], ["#111", "#eee"]],
 
-    "ARG": [["#bdf", "#123"], ["#123", "#fff"]],
-    "AUT": [["#f00", "#fff"], ["#fff", "#034"], ["#000", "#fff"]],
-    "ALG": [["#fff", "#054"], ["#054", "#fff"]],
-    "JOR": [["#fff", "#f00"], ["#f00", "#fff"], ["#000", "#fff"]],
+    "ARG": [["#7ad", "#123"], ["#123", "#eee"]],
+    "AUT": [["#f00", "#eee"], ["#eee", "#034"], ["#111", "#eee"]],
+    "ALG": [["#eee", "#054"], ["#054", "#eee"]],
+    "JOR": [["#eee", "#f00"], ["#f00", "#eee"], ["#111", "#eee"]],
 
     "COL": [["#fd0", "#07e"], ["#236", "#cf3"]],
-    "POR": [["#e02", "#fff"], ["#fff", "#19a"]],
-    "COD": [["#0be", "#fff"], ["#fff", "#0be"], ["#f00", "#ff0"]],
-    "UZB": [["#fff", "#00f"], ["#00f", "#fff"]],
+    "POR": [["#e02", "#eee"], ["#eee", "#19a"]],
+    "COD": [["#0be", "#eee"], ["#eee", "#0be"], ["#f00", "#ff0"]],
+    "UZB": [["#eee", "#00d"], ["#00d", "#eee"]],
 
-    "ENG": [["#fff", "#004"], ["#f11", "#004"]],
-    "CRO": [["#fff", "#00f"], ["#00f", "#fff"]],
-    "GHA": [["#fff", "#000"], ["#fc6", "#000"]],
-    "PAN": [["#f00", "#fff"], ["#fff", "#008"], ["#008", "#fff"]],
+    "ENG": [["#eee", "#004"], ["#f11", "#004"]],
+    "CRO": [["#eee", "#00d"], ["#00d", "#eee"]],
+    "GHA": [["#eee", "#111"], ["#fc6", "#111"]],
+    "PAN": [["#f00", "#eee"], ["#eee", "#228"], ["#228", "#eee"]],
 }
 
 gameType.addEventListener("change", (e) => {
@@ -153,7 +153,7 @@ gameType.addEventListener("change", (e) => {
             break;
 
         case "finals":
-            head.textContent += "CHAMPIONSHIP";
+            head.textContent += "FINALS";
             nav.classList.add("final");
             break;
 
@@ -192,10 +192,18 @@ homeJer.addEventListener("change", (e) => {
     var country = homeTeam.value;
     var jersey = homeJer.value;
 
-    scorebar.forEach((element) => {
-        element.style.backgroundColor = teamVals[country][jersey][0];
-        element.style.color = teamVals[country][jersey][1];
-    })
+    if (jersey === "home") {
+        jersey = 0;
+    } else if (jersey === "road") {
+        jersey = 1;
+    } else if (jersey === "alt") {
+        jersey = 2;
+    } else {
+        alert("Invalid jersey selection. Defaulting to home.");
+        jersey = 0;
+    }
+
+    changeColors(scorebar, country, jersey);
 })
 
 awayJer.addEventListener("change", (e) => {
@@ -205,20 +213,31 @@ awayJer.addEventListener("change", (e) => {
     var country = awayTeam.value;
     var jersey = awayJer.value;
 
-    scorebar.forEach((element) => {
-        element.style.backgroundColor = teamVals[country][jersey][0];
-        element.style.color = teamVals[country][jersey][1];
-    })
+    if (jersey === "home") {
+        jersey = 0;
+    } else if (jersey === "road") {
+        jersey = 1;
+    } else if (jersey === "alt") {
+        jersey = 2;
+    } else {
+        alert("Invalid jersey selection. Defaulting to home.");
+        jersey = 0;
+    }
+
+    changeColors(scorebar, country, jersey);
 })
 
 function changeOptions(homeAway, country) {
     var dropdown;
+    var scorebar;
     const colors = teamVals[country]
 
     if (homeAway === "home") {
         dropdown = homeJer;
+        scorebar = scoreLeft.querySelectorAll(".scorebar");
     } else if (homeAway === "away") {
         dropdown = awayJer;
+        scorebar = scoreRight.querySelectorAll(".scorebar");
     }
 
     if (colors.length === 2) {
@@ -226,4 +245,14 @@ function changeOptions(homeAway, country) {
     } else {
         dropdown.innerHTML = `<option value='home' style="background-color: ${colors[0][0]}; color: ${colors[0][1]}" selected>First Kit</option><option value='road'  style="background-color: ${colors[1][0]}; color: ${colors[1][1]}">Second Kit</option><option value='alt'  style="background-color: ${colors[2][0]}; color: ${colors[2][1]}">Third Kit</option>`
     }
+
+    changeColors(scorebar, country, 0);
+}
+
+function changeColors (scorebar, country, jersey) {
+    console.log("change called.")
+    scorebar.forEach((element) => {
+        element.style.backgroundColor = teamVals[country][jersey][0];
+        element.style.color = teamVals[country][jersey][1];
+    })
 }
